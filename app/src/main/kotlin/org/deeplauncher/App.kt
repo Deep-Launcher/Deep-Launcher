3fd26a7c6668cb12377dca3cf24bb455d35038ed
gofny <214199922+GOFNY@users.noreply.github.com>
2026-09-12 12:37:07 -0300

    feat: implement file setup for launcher directories

--- app/src/main/kotlin/org/deeplauncher/App.kt
+++ app/src/main/kotlin/org/deeplauncher/App.kt
@@ -6,6 +6,36 @@ import javafx.scene.Parent
 import javafx.scene.Scene
 import javafx.scene.image.Image
 import javafx.stage.Stage
+import java.io.File
+
+data class LauncherFiles(
+    val rootDir: File,
+    val instancesDir: File,
+    val librariesDir: File,
+    val assetsDir: File
+)
+
+fun setupFiles(): LauncherFiles {
+    val root = File(System.getProperty("user.home"), ".deeplauncher")
+
+    val assets = File(root, "assets")
+    val assetsIndexes = File(assets, "indexes")
+    val assetsObjects = File(assets, "objects")
+    val libraries = File(root, "libraries")
+    val instances = File(root, "instances")
+
+    assetsIndexes.mkdirs()
+    assetsObjects.mkdirs()
+    libraries.mkdirs()
+    instances.mkdirs()
+
+    return LauncherFiles(
+        rootDir = root,
+        instancesDir = instances,
+        librariesDir = libraries,
+        assetsDir = assets
+    )
+}
 
 class App : Application() {
 
@@ -25,5 +55,6 @@ class App : Application() {
 }
 
 fun main() {
+    val launcherFiles = setupFiles()
     Application.launch(App::class.java)
 }
